96e64d443d0cee5bd941851a595bee5444147949
Tom Owers <owerstom@gmail.com>
2026-07-15 10:52:34 +0100

    chore(data-warehouse): ruff format campfire migration
    Generated-By: PostHog Code
    Task-Id: c5d59a3e-49a8-4d9f-84fe-54e2b9fdf41a

--- products/warehouse_sources/backend/migrations/0070_alter_externaldatasource_source_type_and_more.py
+++ products/warehouse_sources/backend/migrations/0070_alter_externaldatasource_source_type_and_more.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("warehouse_sources", "0069_alter_externaldatasource_created_via"),
     ]
